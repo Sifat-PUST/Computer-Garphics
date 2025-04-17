afbdfd1e8f5176d1e414a431eaf3a0013e2885cc
Md.Sifat <115576448+Sifat-PUST@users.noreply.github.com>
2025-04-17 10:21:29 +0600

    Add files via upload

--- Mid Circle Drawing.py
+++ Mid Circle Drawing.py
@@ -1,50 +1,34 @@
 import  matplotlib.pyplot as plt
-
 def MidCircle(g,f,r):
     p=1-r
+    x=r
+    y=0
 
-    x=g+r
-    y=0+f
-    x_points,y_points=[x,-x,y,-y],[y,-y,x,-x]
+    x_points,y_points=[],[]
 
     i=0
-    while i<r:
+    while y<=x:
+        x_points.extend([x + g, -x+g , x+g ,-x+g,y+g,-y+g,y+g,-y+g])
+        y_points.extend([y+f,y+f,-y+f,-y+f,x+f,x+f,-x+f,-x+f])
+
+        y+=1
         if p<=0:
             x=x
-            y+=1
             p=p+2*y+1
-            x_points.append(x)
-            x_points.append(-x)
-            x_points.append(y)
-            x_points.append(-y)
-            y_points.append(y)
-            y_points.append(-y)
-            y_points.append(x)
-            y_points.append(-x)
         else:
             x = x-1
-            y += 1
             p = p + 2 * y - 2*x+ 1
-            x_points.append(x)
-            x_points.append(-x)
-            x_points.append(y)
-            x_points.append(-y)
-            y_points.append(y)
-            y_points.append(-y)
-            y_points.append(x)
-            y_points.append(-x)
 
     return x_points,y_points
 
-points=[]
-print("Enter endpoints :")
-for i in range(3):
-    c=int(input())
-    points.append(c)
+print("Take g f and r :")
+g=int(input())
+f=int(input())
+r=int(input())
 
-g,f,r = points
 x_coords,y_coords=MidCircle(g,f,r)
 
-plt.plot(x_coords,y_coords)
+plt.scatter(x_coords,y_coords)
+plt.gca().set_aspect('equal', adjustable='box')
 plt.grid(True)
 plt.show()
